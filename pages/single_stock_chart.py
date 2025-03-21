e12611e6c8c2407d55a03fb8470713b470c74394
John Banks <1775095+JcBanks@users.noreply.github.com>
2025-03-21 13:15:42 -0400

    Update single_stock_chart.py

--- pages/single_stock_chart.py
+++ pages/single_stock_chart.py
@@ -59,7 +59,7 @@ def create_price_chart(ticker_data, symbol, start, end):
         title=f"{symbol}",
         xaxis_title="Date",
         yaxis_title="Price",
-        xaxis_range=[start,end],
+        xaxis=dict(range=[start,end]),
         height=400,
         width=800
     )
@@ -95,7 +95,7 @@ with st.form("stock_form"):
                 if data.empty:
                     st.error(f"No data found for symbol '{symbol.upper()}'.")
                 else:
-                    st.success(f"Showing price history for {symbol.upper()}:")
+                    st.success(f"Showing price history for {symbol.upper()} {start} {end}:")
                     st.plotly_chart(create_price_chart(data, symbol.upper(), start, end), use_container_width=True)
                     st.dataframe(data)
                 conn.close()
