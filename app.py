969570e64e9b54c937a2dfc4dee2bcca44485084
John Banks <1775095+JcBanks@users.noreply.github.com>
2025-03-21 12:13:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,18 @@ st.set_page_config(
 st.title("Breakout Monitor Dashboard")
 st.markdown("Welcome to the Project Blizzard Dashboard! Choose one of the monitors below:")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 # ETF Breakout Monitor Tile
 with col1:
-    st.image("assets/Alta_light.png", width=100)
     if st.button("ETF Breakout Monitor"):
         switch_page("etf_breakout_monitor")
 
 # Growth Stock Monitor Tile
 with col2:
-    st.image("assets/Alta_light.png", width=100)
     if st.button("Growth Stock Monitor"):
         switch_page("growth_stock_breakout_monitor")
 
+with col3:
+    if st.button("Single Stock Chart"):
+        switch_page("single_stock_chart")
